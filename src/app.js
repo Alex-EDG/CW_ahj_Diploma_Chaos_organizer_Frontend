971dc411e46ad53665abb0c1e9616d6413b33f8a
Alex-EDG <AlexEDG75@GMail.Com>
2026-05-03 02:21:27 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import Controller from "./controller";
 import Api from "./api";
 import MainView from "./mainView";
 
-// const url = "http://localhost:7070/api";
+// const url = "http://localhost:7075/api";
 const url = "https://cw-ahj-diploma-chaos-organizer-back.vercel.app/api";
 const container = document.querySelector("#root");
 const mainView = new MainView(container);

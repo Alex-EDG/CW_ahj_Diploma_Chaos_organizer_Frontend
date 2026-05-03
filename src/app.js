a82af1953f9045c862d58010c238e5b6c5ccc4c2
Alex-EDG <AlexEDG75@GMail.Com>
2026-05-03 03:23:20 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ import Api from "./api";
 import MainView from "./mainView";
 
 // const url = "http://localhost:7075/api";
-// const url = "https://cw-ahj-diploma-chaos-organizer-back.vercel.app/api";
-const url = "https://cw-ahj-diploma-chaos-organizer-backend.onrender.com/api";
+const url = "https://cw-ahj-diploma-chaos-organizer-back.vercel.app/api";
+// const url = "https://cw-ahj-diploma-chaos-organizer-backend.onrender.com/api";
 const container = document.querySelector("#root");
 const mainView = new MainView(container);
 const api = new Api(url);
